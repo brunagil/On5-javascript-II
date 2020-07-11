53fed53f031a46c8e49e2c90558b15ea019fef07
Denise.L0pes <lopess.denise@gmail.com>
2020-07-10 23:35:58 -0300

    Versão final da ToDoList. Implementas todas as funcionalidades solicitadas. Devido ao prazo, não foi finalizada a adequação dos dados do LocalStorage para o cenário de exclusão de atividade única, este item será incluído na próxima revisão.

--- todolist/deniseLopes/script.js
+++ todolist/deniseLopes/script.js
@@ -1,21 +1,29 @@
 //Selectors
+const header = document.querySelector('.header');
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const todoListShow = document.querySelector('.todo-list-show');
 const alerta = document.querySelector('.message');
 const limpAll = document.querySelector('.fa-power-off');
 const checkAll = document.querySelector('.fa-tasks');
+const completedTask = document.querySelector('.allchecked');
+const btnGuardar = document.querySelector('.fa-eye');
+const btnDeletar = document.querySelector('.fa-dumpster-fire');
+
+
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-// checkAll.addEventListener('click', limpaCheckAll);
+
 
 
 //Functions
 function addTodo(event){
-    const inputTodo = todoInput   
-    if(todoInput.value === ""){
+    const inputTodo = todoInput;
+       
+    if(todoInput.value.trim() === ""){        
         //Esta linha faz com que ele envie a informação
         event.preventDefault();
         //Aqui criamos uma Div como se fosse no HTML
@@ -29,6 +37,7 @@ function addTodo(event){
         inputTodo.focus();
         inputTodo.style.border = "5px solid red"
         alerta.textContent = "Você precisa digitar uma tarefa para adicionar."
+        todoInput.value = "";
     }
     else{
         alerta.textContent = ""
@@ -43,6 +52,8 @@ function addTodo(event){
         newTodo.innerText = todoInput.value;
         newTodo.classList.add("todo-item");
         todoDiv.appendChild(newTodo);
+        //Salvar local
+        saveLocalTodos(todoInput.value)
         //Criar o botão para dar check na atividade.
         const completedButton = document.createElement("button");
         completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
@@ -56,9 +67,10 @@ function addTodo(event){
         //Acrescentar para lista
         todoList.appendChild(todoDiv);
         //Limpar conteúdo digitado.
-        todoInput.value = "";
+        todoInput.value = "";             
     }
 }
+
 
 //Função do botão deletar item
 function deleteCheck(e){
@@ -74,44 +86,78 @@ function deleteCheck(e){
     }
 }
 
+//Limpar todos os itens
 limpAll.addEventListener('click', function(){
-    todoList.remove();
+    const task = document.querySelectorAll('.todo');
+
+    for (let i = 0; i<task.length; i++){
+        todoList.remove();
+        localStorage.clear()
+        window.location.reload();
+    }
 });
 
+//Botão check all
 checkAll.addEventListener('click', function(){
-    todoList.classList.toggle('completed');
-    if(todoInput.value = ""){
-        const newTodoCheck = document.createElement("li");
-        newTodoCheck.innerText = todoInput.value;
-        newTodoCheck.classList.add("todo-item");
-        todoDivCheck.appendChild(newTodoCheck);
+    const task = document.querySelectorAll('.todo');
+      
+    for (let i = 0; i<task.length; i++){
+        if(!task[i].classList.contains('completed')){
+            task[i].classList.toggle('completed');
+            completedTask.style.display = 'block';
+        }
+        else{
+            (task[i].classList.contains('completed'))
+            completedTask.style.display = 'block';
+        }
+    }
+});
+
+//Esta função salva as tarefas digitadas localmente.
+function saveLocalTodos (todo){
+    let todos;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
     }
     else{
-        const newTodo = document.createElement("li");
-        newTodo.innerText = todoInput.value;
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild(newTodo);
+        todos = JSON.parse(localStorage.getItem("todos"));
     }
-    
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+//Esta função remove as tarefas salvas localmente.
+function removeLocalTodos (todo){
+    let todos;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//Esta função pega as informações slavas localmente e imprime na tela.
+btnGuardar.addEventListener('click', function(){
+    const tarefasCheck = document.querySelector('.tarefas');
+    const show = localStorage.getItem("todos");
+    //O replace serve para tirar as aspas ao exibir as tarefas concluídas.          
+    tarefasCheck.textContent = `Tarefas concluídas: ${show.replace(/['"]+/g, '')}`;
 });
 
+//Esta função limpa (exclui permanentemente) as info salvas localmente.
+btnDeletar.addEventListener('click', function(){
+    localStorage.clear()
+    window.location.reload();
+});
 
-// //Função do botão limpar lista
-// function limpaCheckAll(e){
-//     const item = e.target;
-//     for(let i=0; i<classList.length; i++){
-//         if(item.classList[i] === fa-power-off){
-//         const tudo = item.parent.parentElement;
-//         todo.remove();
-//         }
-
-//     }    
-    
-
-// }
-
-
-
-
+const fechar = document.querySelector('.fa-times-circle');
+fechar.addEventListener('click', function(){
+    completedTask.style.display= 'none';
+});
 
 
